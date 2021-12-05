14c068f31dea2db6737ff9e404764f2d5ab3ce33
Salih Demirbas <salih.demirbas@testinium.com>
2021-12-05 22:22:25 +0300

    first step okey

--- specs/example.spec
+++ specs/example.spec
@@ -1,2 +1,8 @@
 Specification Heading
 =====================
+
+login
+-----------------------------------------
+//* "com.ozdilek.ozdilekteyim:id/tv_startShoppingStore" id'li elemente tıkla
+* "3" saniye bekle
+* "com.ozdilek.ozdilekteyim:id/tv_startShoppingStore" id'li elemente tıkla
--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -1,3 +1,27 @@
-public class BasePage extends BaseTest {
+import com.thoughtworks.gauge.Step;
+import org.openqa.selenium.By;
 
+import java.util.concurrent.TimeUnit;
+
+public class BasePage extends BaseTest {
+    private String selectorId;
+
+   /* @Step("<selectorId> id'li elemente tıkla")
+    public void clickById(String selectorId) {
+
+    }*/
+
+
+    @Step("<id> id'li elemente tıkla")
+    public void tikla(String id) {
+        appiumDriver.findElement(By.id(id)).click();
+        //log4j gelmeli
+        System.out.println(selectorId + " elementina tiklandi");
+
+    }
+    @Step("<zaman> saniye bekle")
+    public void bekle(int zaman) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(zaman);
+        System.out.println(zaman + " saniye beklendi.");
+    }
 }
