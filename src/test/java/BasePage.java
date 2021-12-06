e022dd4498bac79ef0c72d4cd31c2ae58138348b
Salih Demirbas <salih.demirbas@testinium.com>
2021-12-06 15:23:05 +0300

    2 scroll

--- src/test/java/BasePage.java
+++ src/test/java/BasePage.java
@@ -46,13 +46,20 @@ public class BasePage extends BaseTest {
     }
     @Step("Scroll Yap")
     public void scroll(){
-        int startx =536;
-        int starty=1616;
-        int endx =536;
-        int endy =359;
+        int startx =558;
+        int starty=1967;
+        int endx =554;
+        int endy =398;
         TouchAction touchAction = new TouchAction(appiumDriver);
         touchAction.longPress(PointOption.point(startx, starty))
                 .moveTo(PointOption.point(endx, endy))
+                .release().perform();
+        int startx_new =546;
+        int starty_new=1963;
+        int endx_new =546;
+        int endy_new =273;
+        touchAction.longPress(PointOption.point(startx_new, starty_new))
+                .moveTo(PointOption.point(endx_new, endy_new))
                 .release().perform();
 
 
